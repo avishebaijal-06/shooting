2014eee67fb65860a919050e9b008bb87324db97
avishebaijal-06 <avishebaijal@gmail.com>
2026-04-05 13:22:46 +0530

    Update shooting2.py

--- shooting2.py
+++ shooting2.py
@@ -1,10 +1,15 @@
 import pgzrun, random,pyautogui
 WIDTH,HEIGHT=pyautogui.size()
+
+vampires=[]
+
 player=Actor("hunter.webp")
 player.pos=WIDTH/2,HEIGHT-200
 def draw():
     screen.blit("bg.jpg",(0,0))
     player.draw()
+    for i in vampires:
+        i.draw()
 
 def update():
     if keyboard.left:
@@ -15,6 +20,19 @@ def update():
         player.x=WIDTH
     if player.x>WIDTH:
         player.x=0
+    # create_vampire()
+    for i in vampires:
+        i.y+=10
+        if i.y>HEIGHT:
+            vampires.remove(i)
+         if i.colliderect(player):
+            vampires.remove(i)     
 
+def create_vampire():
+    v=Actor("vampire.png")
+    v.pos=random.randint(0,WIDTH),0
+    vampires.append(v)
+
+clock.schedule_interval(create_vampire,1)    
 
 pgzrun.go()
